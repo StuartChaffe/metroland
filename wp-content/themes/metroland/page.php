173265a85c848d29e14f0f934e9ed24bcf3f24fb
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-01-20 17:14:31 +0000

    Add page title

--- wp-content/themes/metroland/page.php
+++ wp-content/themes/metroland/page.php
@@ -2,7 +2,16 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<main id="content">
-		<h1><?php the_title(); ?></h1>
+	<?php if ( 'true' == get_field('page_title_show') ) { ?>
+		<div class="page-title">
+		<?php if (get_field('page_title_overwrite')) { ?>
+			<h1 class="uppercase"><?php echo get_field('page_title_overwrite') ?></h1>
+		<?php } else { ?>
+			<h1><?php the_title(); ?></h1>
+		<?php } ?>
+		</div>
+	<?php } ?>
+	
 		<?php the_content(); ?>
 	</main>
 <?php endwhile; ?>
