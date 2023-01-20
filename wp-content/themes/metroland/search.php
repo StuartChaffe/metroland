d4acb74e279f22b6437389dac44bdf63448c743f
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-01-20 19:44:00 +0000

    Style search

--- wp-content/themes/metroland/search.php
+++ wp-content/themes/metroland/search.php
@@ -1,22 +1,33 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ): ?>
+<?php if ( have_posts() ) { ?>
+<?php
+	global $wp_query;
+	$total_results = $wp_query->found_posts;
+?>
 
-<h2>Search Results for '<?php echo get_search_query(); ?>'</h2>
+<div class="page-title">
+	<h1 class="heading-3 uppercase">Search results</h1>
+</div>
 
-<ol class="search-results">
-<?php while ( have_posts() ) : the_post(); ?>
-	<li>
-			<h3><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
-			<?php the_excerpt(); ?>
-	</li>
-<?php endwhile; ?>
-</ol><!-- /search-results -->
+<div class="container">
+	<p>Showing <?php echo $total_results; ?> result(s) for '<?php echo get_search_query(); ?>'</p>
+	<div class="search-results">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<div class="search-results__item">
+				<h4><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>"><?php the_title(); ?></a></h4>
+				<?php the_excerpt(); ?>
+				<br />
+				<a class="btn" href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>">View <?php echo get_post_type(); ?></a>
+			</div>
+		<?php endwhile; ?>
+	</div>
+</div>
 
-<?php else: ?>
+<?php } else { ?>
 	
 <h2>No results found for '<?php echo get_search_query(); ?>'</h2>
 
-<?php endif; ?>
+<?php } ?>
 
 <?php get_footer(); ?>
