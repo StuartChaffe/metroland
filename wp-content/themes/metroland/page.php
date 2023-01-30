b2c5506e5457824bae11d1dd1403753556d90037
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-01-30 22:16:13 +0000

    Style breadcrumb and title

--- wp-content/themes/metroland/page.php
+++ wp-content/themes/metroland/page.php
@@ -2,12 +2,13 @@
 
 <?php while ( have_posts() ) : the_post(); ?>
 	<main id="content">
+	<?php if ( 'true' == get_field('breadcrumb_show') ) { ?><div class="page-breadcrumb"><?php echo do_shortcode('[wpseo_breadcrumb]'); ?></div><?php } ?>
 	<?php if ( 'true' == get_field('page_title_show') ) { ?>
 		<div class="page-title">
 		<?php if (get_field('page_title_overwrite')) { ?>
-			<h1 class="uppercase"><?php echo get_field('page_title_overwrite') ?></h1>
+			<h1 class="uppercase <?php if (get_field('page_title_size') == 'Small') { ?> heading-3<?php } ?>"><?php echo get_field('page_title_overwrite') ?></h1>
 		<?php } else { ?>
-			<h1 class="uppercase"><?php the_title(); ?></h1>
+			<h1 class="uppercase <?php if (get_field('page_title_size') == 'Small') { ?> heading-3<?php } ?>"><?php the_title(); ?></h1>
 		<?php } ?>
 		<?php if (has_excerpt()) { ?><div class="page-excerpt"><?php the_excerpt(); ?></div><?php } ?>
 		</div>
