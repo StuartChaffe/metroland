b4f8e4dbea7e67a657d7f0134b8b68039cd9ac33
Stuart Chaffe <stu@stuartchaffe.co.uk>
2023-10-11 10:56:45 +0100

    Fix login logo

--- wp-content/themes/metroland/functions.php
+++ wp-content/themes/metroland/functions.php
@@ -34,7 +34,7 @@ require_once( 'blocks/register.php' );
 function my_login_logo() { ?>
 	<style type="text/css">
 		#login h1 a, .login h1 a {
-            background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/src/images/logo.png');
+            background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/src/images/logo.svg');
 			height: 146px;
 			width: 250px;
             background-size: 250px 146px;
